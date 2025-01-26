6891754e0d895c95c2b9b97efdba174a94a92331
deepeshiitjaiswal <deepeshiitjaiswal786@gmail.com>
2025-01-26 09:51:49 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: '*',
   methods: ['GET', 'POST', 'OPTIONS'],
   credentials: true
 }));
